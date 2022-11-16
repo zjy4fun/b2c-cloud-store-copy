d547fa5b32c081ae81f5b2c28012f75a864b878d
zjy4fun <zjy4fun@163.com>
2022-11-16 09:02:15 +0800

    静态资源配置文件

---
+++ store-static/src/main/java/org/example/StaticOssApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticOssApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StaticOssApplication.class, args);
+    }
+}
