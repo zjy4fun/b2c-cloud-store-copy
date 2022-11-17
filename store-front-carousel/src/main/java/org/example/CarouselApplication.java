71f7a0ee08e2ca99bcb9956ca45673c9ac8477e3
zjy4fun​ <zjy4fun@163.com>
2022-11-18 00:52:49 +0800

    轮播图服务初始化完成

---
+++ store-front-carousel/src/main/java/org/example/CarouselApplication.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(basePackages = "org.example.mapper")
+public class CarouselApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarouselApplication.class,args);
+    }
+}
